e04360366de9e376881368ad23caa39ae36681a0
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-02 18:24:14 +0500

    update for lessons02

--- dz3.py
+++ dz3.py
@@ -1,3 +1,18 @@
-number = int(input('Введите число: '))
-amount = (number + (number * 11)  + (number * 111))
-print(amount)
+# Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна,
+# лето, осень).
+# Напишите решения через list и через dict.
+
+
+seasons_dict = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
+month = int(input('Введите номер месяца: '))
+if month == 12 or month == 1 or month == 2:
+    print(seasons_dict.get(1))
+elif month == 3 or month == 4 or month == 5:
+    print(seasons_dict.get(2))
+elif month == 6 or month == 7 or month == 8:
+    print(seasons_dict.get(3))
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(4))
+else:
+    print("Такого месяца не существует")
