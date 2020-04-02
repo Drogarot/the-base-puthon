7c46c735db811f349f4a7c34006b46ba5b038a83
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-02 18:21:34 +0500

    update 2

--- dz1.py
+++ dz1.py
@@ -1,4 +1,14 @@
-age = int(input('Сколько вам лет?: '))
-name = input('Как вас зовут?: ')
-patronymic = input('Введите ваше отчество: ')
-print('Здравствуйте! ', name, patronymic)
+# Создать список и заполнить его элементами различных типов данных.
+# Реализовать скрипт проверки типа данных каждого элемента. Использовать функцию type() для проверки типа.
+# Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
+
+whole = 5
+fractional = 1.2
+line = 'First race'
+leaflet = ['c', '4']
+my_tuple = ('2', '8')
+my_dict = {'name': 'Anna', 'surname': 'Federovna'}
+
+my_list = [whole, fractional, line, leaflet, my_tuple, my_dict]
+for i in my_list:
+    print(f'{i} is {type(i)}')
