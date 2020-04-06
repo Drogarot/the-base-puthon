75cb48abf6cb62d26f1254d7c5610a181bcd351c
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-07 01:30:51 +0500

    update3 dz3

--- dz3.py
+++ dz3.py
@@ -1,3 +1,13 @@
-number = int(input('Введите число: '))
-amount = (number + (number * 11)  + (number * 111))
-print(amount)
+# Реализовать функцию my_func(), которая принимает три позиционных
+# аргумента, и возвращает сумму наибольших двух аргументов.
+
+def my_func(arg1, arg2, arg3):
+    if arg1 >= arg2 and arg2 >= arg3:
+        return arg1 + arg2
+    elif arg1 > arg2 and arg1 < arg3:
+        return arg1 + arg3
+    else:
+        return arg2 + arg3
+
+
+print(f'Результат {my_func(int(input("arg1 ")), int(input("arg2 ")), int(input("arg3 ")))}')
