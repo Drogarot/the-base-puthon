fe87609b9fd5504173b77ee4aac0fa23cd9bb315
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-07 01:30:05 +0500

    update3 dz2

--- dz2.py
+++ dz2.py
@@ -1,6 +1,11 @@
-time_sec = int(input('Введите время в секундах: '))
-hours = time_sec // 3600
-residue = time_sec % 3600
-minutes = residue // 60
-sec = residue % 60
-print(f'Введёное вами время: {hours}:{minutes}:{sec}')
+# Реализовать функцию, принимающую несколько параметров,
+# описывающих данные пользователя: имя, фамилия, год рождения,
+# город проживания, email, телефон. Функция должна принимать
+# параметры как именованные аргументы. Реализовать вывод данных
+# о пользователе одной строкой.
+
+def user_data_func(name, surname, age, email, city, number_phone):
+   print(name, surname, age, email, city, number_phone)
+
+
+user_data_func(name='Ivan', surname='Vasilevich', age=30, email='mail.ru', city='Karpinsk', number_phone='8-800-3000-500')
