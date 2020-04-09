0e0f5a8190c48bc7e8bdcc711744e9873356b940
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-09 22:23:07 +0500

    update4 dz1

--- dz1.py
+++ dz1.py
@@ -1,4 +1,19 @@
-age = int(input('Сколько вам лет?: '))
-name = input('Как вас зовут?: ')
-patronymic = input('Введите ваше отчество: ')
-print('Здравствуйте! ', name, patronymic)
+# Реализовать скрипт, в котором должна быть предусмотрена
+# функция расчета заработной платы сотрудника. В расчете
+# необходимо использовать формулу:
+# (выработка в часах*ставка в час) + премия.
+# Для выполнения расчета для конкретных значений необходимо
+# запускать скрипт с параметрами.
+
+from sys import argv
+
+name, time, salary, premium = argv
+
+try:
+    time = float(time)
+    salary = float(salary)
+    premium = float(premium)
+    res = time * salary + premium
+    print(f'Заработная плата сотрудника  {res}')
+except ValueError:
+    print('Not a number')
