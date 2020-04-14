30561399ce7ef13c417ccebe6e464c4729616f34
Ринат Халиуллин <61630147+Drogarot@users.noreply.github.com>
2020-04-14 15:32:34 +0500

    delete dz6

--- dz6.py
+++
@@ -1,9 +0,0 @@
-one_day = float(input('Какой ваш результат в первый день: '))
-result = float(input('Чего вы хотите достичь: '))
-day = 1
-if one_day > result:
-    print(day)
-while one_day < result:
-    one_day = one_day + one_day%10
-    day += 1
-print(day)
